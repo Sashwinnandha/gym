7469da18bc2aadbc7bcf4d5e842ee9c8db9fdb8b
Sashwin <sashwincareer@gmail.com>
2024-10-30 21:41:03 +0530

    Third Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import{} from 'ngx-bootstrap';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
 
